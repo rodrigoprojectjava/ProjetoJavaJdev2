e7270488060ec347e325ca04967f9f0b6826b583
Rodrigo Oliveira Santos <projetosrodrigojava@gmail.com>
2024-01-22 18:02:08 -0300

    Criado o campo dataNascimento

--- ProjectJavaJdev2/src/JdevJava/Pessoa.java
+++ ProjectJavaJdev2/src/JdevJava/Pessoa.java
@@ -1,11 +1,20 @@
 package JdevJava;
 
+import java.util.Date;
+
 public class Pessoa {
 	
 	private String nome;
 	private String sobrenome;
 	private int idade;
+	private Date dataNascimento;
 	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 	public int getIdade() {
 		return idade;
 	}
